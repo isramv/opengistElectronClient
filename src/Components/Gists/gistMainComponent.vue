c039d1758e525e605910d00e8516438cd0646b39
Israel Morales <israel.morales@rocket.com.mx>
2016-10-25 23:14:45 -0700

    Incremental

--- src/Components/Gists/gistMainComponent.vue
+++ src/Components/Gists/gistMainComponent.vue
@@ -35,55 +35,64 @@
     <div class="row">
         <div class="col-xs-4 col-sm-4 col-md-3 table-container">
             <a href="#" @click="fetchGists()">update index</a>
-            <!--<table-gist></table-gist>-->
+            <table-gist></table-gist>
         </div>
         <div class="col-xs-8 col-sm-8 col-md-9 show-gist-container">
             <div class="rc-container">
                 <a href="#" class="btn btn-default btn-sm" @click="changeState('new')">New</a>
             </div>
-            <div v-show="state == 'view'">
-                <show-gist></show-gist>
-            </div>
-            <div v-show="state == 'edit'">
-                <edit-gist></edit-gist>
-            </div>
-            <div v-show="state == 'new'">
-                <new-gist></new-gist>
-            </div>
+            <!--<div v-show="state == 'view'">-->
+                <!--<show-gist></show-gist>-->
+            <!--</div>-->
+            <!--<div v-show="state == 'edit'">-->
+                <!--<edit-gist></edit-gist>-->
+            <!--</div>-->
+            <!--<div v-show="state == 'new'">-->
+                <!--<new-gist></new-gist>-->
+            <!--</div>-->
         </div>
     </div>
 </template>
 <script>
     import _ from 'lodash'
-    import store from '../store'
-    import router from '../router'
-    import showGistComponent from './showGist.vue'
-    import editGistComponent from './editGist.vue'
+    import store from '../vuex_store'
+//    import showGistComponent from './showGist.vue'
+//    import editGistComponent from './editGist.vue'
     import tableGistComponent from './tableGist.vue'
-    import newGistComponent from './newGist.vue'
+//    import newGistComponent from './newGist.vue'
     import keymaster from 'keymaster'
     export default{
         components: {
-//            'table-gist': tableGistComponent,
-            'show-gist': showGistComponent,
-            'edit-gist': editGistComponent,
-            'new-gist': newGistComponent
+            'table-gist': tableGistComponent,
+//            'show-gist': showGistComponent,
+//            'edit-gist': editGistComponent,
+//            'new-gist': newGistComponent
         },
-        data() {
-            return store
-        },
-        beforeCompile() {
-            let self = this;
-            if(localStorage.getItem('gists') !== null) {
-                this.$set('gists', JSON.parse(localStorage.getItem('gists')));
-            } else {
-                this.fetchGists();
+        store: store,
+        computed: {
+            username() {
+                return this.$store.state.username
+            },
+            auth() {
+                return this.$store.state.auth
             }
+        },
+        beforeCreate() {
+            if(localStorage.username !== null) {
+                this.$store.commit('USERNAME', localStorage.username)
+            }
+            if(localStorage.auth !== null) {
+                this.$store.commit('ACCESSTOKEN', localStorage.auth)
+            }
+        },
+        created() {
+            let self = this
+            this.fetchGists()
             keymaster('command+n', function() {
-                self.changeState('new');
+                self.changeState('new')
             });
             keymaster('command+escape', function() {
-                self.changeState('view');
+                self.changeState('view')
             });
             keymaster('command+e', function() {
                 let gid = store.gist.id;
@@ -91,61 +100,61 @@
                 self.editGist(gid);
             });
         },
-        events: {
-            'logout-all': function() {
-                var self = this;
-                self.$set('gists', null);
-                self.$set('state', '');
-                self.$broadcast('logout-global');
-                router.go('login');
-            },
-            'view-gist': function(gist) {
-                this.showGist(gist);
-            },
-            'edit-gist': function(gistId) {
-                this.editGist(gistId);
-            },
-            'update-all': function() {
-                this.fetchGists();
-            },
-            'update-gist-on-index': function(gist) {
-                let gid = gist.id;
-                console.log(store.gists);
-                // todo is going to search the gist by id and update with the new one.
-                _.find(store.gists, function(g) {
-                    if(g.gist.id == gid) {
-                        g.gist = gist;
-                    }
-                });
-            }
-        },
+//        events: {
+//            'logout-all': function() {
+//                var self = this;
+//                self.$set('gists', null);
+//                self.$set('state', '');
+//                self.$broadcast('logout-global');
+//                router.go('login');
+//            },
+//            'view-gist': function(gist) {
+//                this.showGist(gist);
+//            },
+//            'edit-gist': function(gistId) {
+//                this.editGist(gistId);
+//            },
+//            'update-all': function() {
+//                this.fetchGists();
+//            },
+//            'update-gist-on-index': function(gist) {
+//                let gid = gist.id;
+//                console.log(store.gists);
+//                // todo is going to search the gist by id and update with the new one.
+//                _.find(store.gists, function(g) {
+//                    if(g.gist.id == gid) {
+//                        g.gist = gist;
+//                    }
+//                });
+//            }
+//        },
         methods: {
             fetchGists() {
                 let self = this;
                 $.ajax({
                     url: 'http://myapp.local/app_dev.php/api/v1/gists',
-                    headers: { 'authorization': localStorage.getItem('Authorization') },
+                    headers: { 'authorization': self.auth },
                     type: 'GET'
                 }).done(function(res) {
-                    self.$set('gists', res);
-                    self.$get('gists');
-                    localStorage.setItem('gists', JSON.stringify(res));
+//                    self.$set('gists', res);
+//                    self.$get('gists');
+//                    localStorage.setItem('gists', JSON.stringify(res));
                 });
             },
-            changeState(state) {
-                let self = this;
-                self.$set('state', state);
-                if(state === 'new') {
-                    self.$broadcast('new-gist');
-                }
-            },
-            showGist(gist) {
-                this.$set('state', 'view');
-                this.$broadcast('view-gist', gist);
-            },
-            editGist(gistId) {
-                this.$broadcast('edit-gist', gistId);
-            }
+//            changeState(state) {
+//                let self = this;
+//                self.$set('state', state);
+//                if(state === 'new') {
+//                    self.$broadcast('new-gist');
+//                }
+//            },
+//            showGist(gist) {
+//                this.$set('state', 'view');
+//                this.$broadcast('view-gist', gist);
+//            },
+//            editGist(gistId) {
+//                this.$broadcast('edit-gist', gistId);
+//            }
         }
     }
 </script>
