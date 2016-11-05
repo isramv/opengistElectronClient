b668e0e6a5a5f28d404a8c2261eb12cbf4469eb8
Israel Morales <israel.morales@rocket.com.mx>
2016-11-04 22:45:42 -0700

    first attempt to prevent external links

--- src/Components/Gists/viewGist.vue
+++ src/Components/Gists/viewGist.vue
@@ -90,7 +90,36 @@
       },
       gistRender () {
           if(this.$store.state.viewGist.gist.body.length > 0) {
-              return marked(this.$store.state.viewGist.gist.body)
+
+              let body = this.$store.state.viewGist.gist.body;
+
+              let filtered = body.replace(/<a/, '<a target="_blank"');
+
+              let renderer = new marked.Renderer();
+              renderer.link = function(href, title, text) {
+                if (this.options.sanitize) {
+                  try {
+                    var prot = decodeURIComponent(unescape(href))
+                            .replace(/[^\w:]/g, '')
+                            .toLowerCase();
+                  } catch (e) {
+                    return '';
+                  }
+                  if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) {
+                    return '';
+                  }
+                }
+                var out = '<a target="_blank" class="link-view-gist" href="' + href + '"';
+                if (title) {
+                  out += ' title="' + title + '"';
+                }
+                out += '>' + text + '</a>';
+                return out;
+              }
+
+              return marked(filtered, { renderer: renderer})
+
+
           } else {
               return ''
           }
